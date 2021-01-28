c990a4ac4bd14100a338c339fff71f27f69da2d7
Savagebeanman <72805052+Savagebeanman@users.noreply.github.com>
2021-01-28 09:09:18 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+while True:
+    print(input.acceleration(Dimension.Z))
+    if input.acceleration(Dimension.Z) > 0:
+        music.ba_ding.play_until_done()
---
+++ main.ts
@@ -0,0 +1,7 @@
+while (true) {
+    console.log(input.acceleration(Dimension.Z))
+    if (input.acceleration(Dimension.Z) > 0) {
+        music.baDing.playUntilDone()
+    }
+    
+}
